2bbbb425594e11eb5bdeafe77b42fff8c6224f0b
Yousef Abdelkhaleq <yousefbacarro@icloud.com>
2023-02-16 15:18:07 -0500

    dewedeth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -59,7 +59,7 @@ export default function WithSubnavigation() {
             color={useColorModeValue("gray.800", "white")}
           >
             <Button as="a" href="/" variant={"link"}>
-              Logo
+              DailyDev
             </Button>
           </Text>
 
